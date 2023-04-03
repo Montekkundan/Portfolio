cca82602addddd30e512fd14b643d0b0f7af4f8a
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-04-02 18:00:12 -0700

    update

--- Experience/Experience.tsx
+++ Experience/Experience.tsx
@@ -6,16 +6,17 @@ import { useRef } from 'react';
 import { Mesh } from 'three/src/objects/Mesh';
 import * as THREE from 'three';
 import OperatingSystem from '../components/OperatingSystem';
+import gsap from 'gsap';
 
 
 function Experience(enable: any): JSX.Element {
-  console.log(enable);
+  
   const model = useLoader(GLTFLoader, '/model/room.glb');
   const { camera } = useThree();
   let time = 0;
-const duration = 5; // duration of the animation in seconds
-const start = new THREE.Vector3(0, 2.2828, 3.4258);
-const end = new THREE.Vector3(-3.5359259, 2.2828, 3.4258);
+  const duration = 5; // duration of the animation in seconds
+  const start = new THREE.Vector3(0, 2.2828, 3.4258);
+  const end = new THREE.Vector3(-3.5359259, 2.2828, 3.4258);
 
 
 function easeInOutCubic(t: number) {
@@ -35,6 +36,16 @@ useFrame(({ clock }) => {
   // setInterval(() => {
   //   console.log('Camera position:', camera.position);
   // }, 1000);
+  if (!enable.enable) {
+    // Use a sine wave function to move the camera left and right slowly
+    // Adjust the amplitude and frequency as needed
+    const amplitude = 1; // The maximum distance from the center
+    const frequency = 0.5; // The number of cycles per second
+    const x = camera.position.x; // The x coordinate changes over time
+    const y = camera.position.y; // The y coordinate stays the same
+    const z = amplitude * Math.sin(frequency * elapsed); // The z coordinate stays the same
+    camera.position.set(x, y, z); // Set the new camera position
+  }
 });
 
   camera.lookAt(0, 0.7, 14); // Make camera point at point of interest
@@ -52,15 +63,20 @@ useFrame(({ clock }) => {
   const handleBoxPointerOut = () => {
     document.body.style.cursor = 'auto';
   };
+  var iframe = document.querySelector('#webpage');
+  console.log(iframe)
+
 
   return (
     <>
       
       <OrbitControls
+      enableRotate={enable.enable}
         minDistance={2}
-        maxDistance={8}
+        maxDistance={6}
         maxPolarAngle={Math.PI / 2.1}
         position={[-4, 2.5, 5]}
+        enablePan={false}
       />
      <directionalLight
       castShadow
@@ -122,10 +138,10 @@ useFrame(({ clock }) => {
     wrapperClass="htmlScreen"
     distanceFactor={ 0.22 }
     rotation={[0, Math.PI, 0]}
-    position={ [ 0, 0.686, 0.12] }
+    position={ [ 0.295, 0.87, 0.12] }
     rotation-x={  0 }
     >
-      { enable && <iframe src="https://www.montek.dev/terminal/" />}
+      <iframe id='webpage' src="https://www.montek.dev/terminal/" />
           
           {/* <OperatingSystem/> */}
       </Html>
