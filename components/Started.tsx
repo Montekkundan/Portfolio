2a8a990386a0261ba2641659fd7a8d8c9b4472e5
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-06-12 13:33:33 -0700

    Update Started.tsx

--- components/Started.tsx
+++ components/Started.tsx
@@ -6,25 +6,10 @@ import original from 'react95/dist/themes/original'
 // @ts-ignore
 
 
+
 import { Title } from './Title'
 import { Window } from './Window'
 
-const GlobalStyles: any = createGlobalStyle`
-  @font-face {
-    font-family: 'ms_sans_serif';
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    font-weight: bold;
-    font-style: normal
-  }
-  body, input, select, textarea {
-    font-family: 'ms_sans_serif';
-  }
-  ${styleReset}
-`
 
 const PAGE_TITLES = ["And it", 'all', 'started', 'with', ' a ...']
 
@@ -33,7 +18,6 @@ const ThemeProviderProxy: any = ThemeProvider
 export default function Started() {
   return (
     <div>
-      <GlobalStyles />
       <ThemeProviderProxy theme={original}>
         {PAGE_TITLES.map(title => (
           <div className='h-screen flex justify-center items-center text-[20vw] font-extrabold font-mono' key={title}>
